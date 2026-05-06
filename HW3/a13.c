9c97800d3f106478028e5107f6341cd16ed504f0
Alexander Danilenko <alexander12091992@gmail.com>
2026-05-06 17:09:32 +0300

    HW3/13

---
+++ HW3/a13.c
@@ -0,0 +1,42 @@
+/*
+ * test.c
+ * 
+ * Copyright 2026 Akarna <Akarna@DESKTOP-82JJA0E>
+ * 
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ * 
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston,
+ * MA 02110-1301, USA.
+ * 
+ * 
+ */
+
+
+#include <stdio.h>
+
+int main()
+{ 
+	int a;
+	
+	scanf("%d", &a);
+	
+	int a1 = a / 100;
+	int a2 = (a / 10) % 10;
+	int a3 = a % 10;
+	
+	int mp = a1 * a2 * a3;
+	
+	printf("%d\n", mp);
+		
+	return 0;
+}
